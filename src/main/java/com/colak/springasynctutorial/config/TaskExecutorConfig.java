a960688057d004211cc60d2621a123fa94729fb2
Orçun Çolak <orcun.colak@hazelcast.com>
2024-02-07 13:57:00 +0300

    Set rejection handler example

--- src/main/java/com/colak/springasynctutorial/config/TaskExecutorConfig.java
+++ src/main/java/com/colak/springasynctutorial/config/TaskExecutorConfig.java
@@ -1,22 +1,30 @@
 package com.colak.springasynctutorial.config;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.scheduling.concurrent.ThreadPoolTaskExecutor;
 
 import java.util.concurrent.Executor;
+import java.util.concurrent.ThreadPoolExecutor;
 
 @Configuration
+@Slf4j
 public class TaskExecutorConfig {
 
     @Bean
     public Executor taskExecutor() {
-        ThreadPoolTaskExecutor executor = new ThreadPoolTaskExecutor();
-        executor.setCorePoolSize(1);
-        executor.setMaxPoolSize(5);
-        executor.setQueueCapacity(500);
-        executor.setThreadNamePrefix("GithubLookup-");
-        executor.initialize();
-        return executor;
+        ThreadPoolTaskExecutor threadPoolTaskExecutor = new ThreadPoolTaskExecutor();
+        threadPoolTaskExecutor.setCorePoolSize(1);
+        threadPoolTaskExecutor.setMaxPoolSize(5);
+        threadPoolTaskExecutor.setQueueCapacity(500);
+        threadPoolTaskExecutor.setThreadNamePrefix("MyAsyncThread-");
+        threadPoolTaskExecutor.setRejectedExecutionHandler(this::logRejection);
+        threadPoolTaskExecutor.initialize();
+        return threadPoolTaskExecutor;
+    }
+
+    private void logRejection(Runnable runnable, ThreadPoolExecutor taskExecutor) {
+        log.warn("Task rejected, thread pool is full and queue is also full");
     }
 }
