aab2e40d1aa5de29d943ff01eec7a3e013398f30
Orçun Çolak <orcun.colak@hazelcast.com>
2024-02-07 17:28:04 +0300

    AsyncSupportConfigurer example

---
+++ src/main/java/com/colak/springasynctutorial/config/WebMvcConfigurerConfig.java
@@ -0,0 +1,28 @@
+package com.colak.springasynctutorial.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.scheduling.concurrent.ThreadPoolTaskExecutor;
+import org.springframework.web.servlet.config.annotation.AsyncSupportConfigurer;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+/**
+ * See <a href="https://howtodoinjava.com/spring-boot/spring-async-controller-responsebodyemitter/">...</a>
+ */
+@Configuration
+public class WebMvcConfigurerConfig implements WebMvcConfigurer {
+
+    @Override
+    public void configureAsyncSupport(AsyncSupportConfigurer configurer) {
+        configurer.setTaskExecutor(mvcTaskExecutor());
+        configurer.setDefaultTimeout(30_000);
+    }
+
+    @Bean
+    public ThreadPoolTaskExecutor mvcTaskExecutor() {
+        ThreadPoolTaskExecutor threadPoolTaskExecutor = new ThreadPoolTaskExecutor();
+        threadPoolTaskExecutor.setCorePoolSize(10);
+        threadPoolTaskExecutor.setThreadNamePrefix("mvc-task-");
+        return threadPoolTaskExecutor;
+    }
+}
